0528c9c56e29897a732c43f432b4d4df5cd95d79
rzshaikh9428-alt <rzshaikh9428@gmail.com>
2026-01-16 19:28:58 +0530

    Add files via upload

---
+++ middleware/auth.js
@@ -0,0 +1,23 @@
+const jwt = require("jsonwebtoken");
+
+module.exports = function (req, res, next) {
+  const header = req.headers.authorization;
+
+  if (!header) {
+    return res.status(401).json({ message: "No token" });
+  }
+
+  const token = header.split(" ")[1];
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    if (decoded.role !== "admin") {
+      return res.status(403).json({ message: "Not admin" });
+    }
+
+    next();
+  } catch (err) {
+    res.status(401).json({ message: "Invalid token" });
+  }
+};
