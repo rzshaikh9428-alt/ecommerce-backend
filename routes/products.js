e19766f798bbd69d97c4148771928d2d0f20429e
rzshaikh9428-alt <rzshaikh9428@gmail.com>
2026-01-16 18:11:45 +0530

    Add files via upload

---
+++ routes/products.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+
+let products = [
+  { _id: "1", name: "Laptop", price: 50000 },
+  { _id: "2", name: "Phone", price: 20000 }
+];
+
+router.get("/", (req, res) => {
+  res.json(products);
+});
+
+router.post("/", (req, res) => {
+  products.push(req.body);
+  res.json({ status: "added" });
+});
+
+module.exports = router;
