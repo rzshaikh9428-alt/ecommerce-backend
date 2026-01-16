6482b61d79070e02ec7da9e63907310302759b3f
rzshaikh9428-alt <rzshaikh9428@gmail.com>
2026-01-16 18:22:08 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
+
+/* 🔥 THIS LINE IS REQUIRED 🔥 */
+app.use("/api/products", require("./routes/products"));
+app.use("/api/auth", require("./routes/auth"));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log("Backend running on port", PORT);
+});
